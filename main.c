473f50ffaecd1a59d777cb7fa3753f41cd64215e
Bruno Marques Bastos <bruno.ufrgs2019@gmail.com>
2023-05-08 10:24:34 -0300

    Edit read of path

--- main.c
+++ main.c
@@ -3,22 +3,30 @@
 int main(int argc, char const *argv[])
 {
     FILE *file;
-    char path[200] = "C:/Users/Bruno/Desktop/CProjects/otimizacao/instancias/";
+    char path[256] = "";
     char file_name[] = "instancia_5_5.dat";
-    strcat(path, file_name);
-    int *vector_of_parameters;
-    int **matrix_of_tasks;
 
-    vector_of_parameters = get_data_from_file(path);
+    if (getcwd(path, sizeof(path)) == NULL){
+        perror("getcwd() error\n");
+    }
+    else {
+        strcat(path, "\\instancias\\");
+        strcat(path, file_name);
+        int *vector_of_parameters;
+        int **matrix_of_tasks;
 
-    int n_tasks = vector_of_parameters[0];
-    int n_cores = vector_of_parameters[1];
+        vector_of_parameters = get_data_from_file(path);
 
-    matrix_of_tasks = build_task_matrix(vector_of_parameters, n_tasks);
-    free(vector_of_parameters);
-    Solution initial_solution = build_initial_solution(n_tasks);
-    printf("Jobs: %d = Valor: %d\n", n_tasks, initial_solution.value);
-    print_solution_matrix(initial_solution, n_tasks);
+        int n_tasks = vector_of_parameters[0];
+        int n_cores = vector_of_parameters[1];
+
+        matrix_of_tasks = build_task_matrix(vector_of_parameters, n_tasks);
+        free(vector_of_parameters);
+        Solution initial_solution = build_initial_solution(n_tasks);
+        printf("Jobs: %d = Valor: %d\n", n_tasks, initial_solution.value);
+        print_solution_matrix(initial_solution, n_tasks);
+    }
+
 
 
     return 0;
